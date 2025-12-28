8c06ea54cfbdf6eb799602d19bc9aa2e3418a5c6
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-28 21:55:46 +0300

    basic design is complete.

---
+++ digitalClock.py
@@ -0,0 +1,17 @@
+from tkinter import Label, Tk
+import time
+
+app_windows = Tk()
+app_windows.title("Digital Clock")
+app_windows.geometry("500x500")
+app_windows.resizable(0,0)
+app_windows.config(bg="Black")
+
+
+
+
+
+
+
+
+app_windows.mainloop()
