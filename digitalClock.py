9872b5246d4fdba1398f503ea8d6180b2ea51af7
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-28 22:31:28 +0300

    Digital Clock is completed.

--- digitalClock.py
+++ digitalClock.py
@@ -1,17 +1,32 @@
 from tkinter import Label, Tk
 import time
 
-app_windows = Tk()
-app_windows.title("Digital Clock")
-app_windows.geometry("500x500")
-app_windows.resizable(0,0)
-app_windows.config(bg="Black")
+app_window = Tk()
+app_window.title("Digital Clock")
+app_window.geometry("250x95")
+app_window.resizable(0,0)
+app_window.config(bg="Black")
 
+text_font = ("Boulder",36,'bold')
+background = "black"
+foreground = "white"
+border_witdh = 20
 
+# Clock
+clock_label = Label(app_window, font=("Boulder",18), bg=background, fg=foreground)
+clock_label.grid(row=0,column=1,padx=10,pady=10)
+# Date
+date_label = Label(app_window, font=("Boulder",18), bg=background, fg=foreground)
+date_label.grid(row=1,column=1,padx=10,pady=10)
 
+def digital_clock():
+    # Clock
+    time_info = time.strftime("%H:%M:%S")
+    clock_label.config(text=time_info)
+    # Date
+    date_info = time.strftime("%d %B %Y")
+    date_label.config(text=date_info)
+    clock_label.after(200,digital_clock)
 
-
-
-
-
-app_windows.mainloop()
+digital_clock()
+app_window.mainloop()
