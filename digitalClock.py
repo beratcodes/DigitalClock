09c2e53b61c06dca0a7d0ae7b7edac1f5ed8a008
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-29 00:14:41 +0300

    code updated.

--- digitalClock.py
+++ digitalClock.py
@@ -3,20 +3,19 @@ import time
 
 app_window = Tk()
 app_window.title("Digital Clock")
-app_window.geometry("250x95")
+app_window.geometry("490x200")
 app_window.resizable(0,0)
 app_window.config(bg="Black")
 
 text_font = ("Boulder",36,'bold')
 background = "black"
 foreground = "white"
-border_witdh = 20
 
 # Clock
-clock_label = Label(app_window, font=("Boulder",18), bg=background, fg=foreground)
+clock_label = Label(app_window, font=text_font, bg=background, fg=foreground)
 clock_label.grid(row=0,column=1,padx=10,pady=10)
 # Date
-date_label = Label(app_window, font=("Boulder",18), bg=background, fg=foreground)
+date_label = Label(app_window, font=text_font, bg=background, fg=foreground)
 date_label.grid(row=1,column=1,padx=10,pady=10)
 
 def digital_clock():
